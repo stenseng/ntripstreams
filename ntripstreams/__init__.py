c835389ad719ca1cf0e4ddbcba1a62a871d45c9b
Lars Stenseng <lars@stenseng.net>
2021-06-11 09:28:21 +0200

    More cleanup

--- ntripstreams/__init__.py
+++ ntripstreams/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["NtripStream", "Rtcm3", "crc24q", "crcNmea"]
 
+from ntripstreams.crc import crc24q, crcNmea
 from ntripstreams.ntripstreams import NtripStream
 from ntripstreams.rtcm3 import Rtcm3
-from ntripstreams.crc import crc24q, crcNmea
