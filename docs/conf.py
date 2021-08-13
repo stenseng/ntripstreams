8aebb695cd0dfe66960a6968cb1d23514d75add0
Lars Stenseng <lars@stenseng.net>
2021-08-13 14:20:38 +0200

    Auto docs, cont.

--- docs/conf.py
+++ docs/conf.py
@@ -152,7 +152,9 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "ntripstreamsapi", "ntripstreamsapi Documentation", [author], 1)]
+man_pages = [
+    (master_doc, "ntripstreamsapi", "ntripstreamsapi Documentation", [author], 1)
+]
 
 
 # -- Options for Texinfo output ----------------------------------------------
